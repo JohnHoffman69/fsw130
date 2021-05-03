0908e97a60c1ba97cb33c73f59dc9f2a9b67e09c
JohnHoffman69 <john.hoffman@bryanuniversity.edu>
2021-05-02 22:37:24 -0700

    stopwatch

---
+++ week3/stop-watch/src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+function Header() {
+    return (
+        <header className='header'>
+            <img className='image' src='https://images.unsplash.com/photo-1508962914676-134849a727f0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDJ8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=400&q=60' alt='logo'/>
+            <h2 className='title'>Stopwatch</h2>
+        </header>
+    )
+}
+
+export default Header
